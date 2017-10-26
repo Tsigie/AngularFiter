010371545d62aa740c2b812f5c33b345ac3e9f7a
Tsigie <tktsigie@yahoo.com>
2017-10-26 13:01:36 -0400

    updated files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+$scope.f = {};
+
+     $scope.filter_by = function(field) {
+       if ($scope.g[field] === '') {
+            delete $scope.f['__' + field];
+            return;
+       }
+       $scope.f['__' + field] = true;
+       $scope.classMates.forEach(function(v) { v['__' + field] = v[field] < $scope.g[field]; })
+     }
